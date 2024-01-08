45d8c1ce155bcc3038746dcb460480d8cf53abfb
Abby <abigailbernabei@Abigails-Air.lan>
2024-01-07 22:33:56 -0600

    feat: select a featured puppy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import { useState } from "react";
 import { puppyList } from "./data.js";
+import "./path-to-css.css";
 import "./App.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log(puppies);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log("feat pup", featuredPup);
   return (
     <div>
       {puppies.map((puppy) => {
-        return <p>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
+      {featPupID && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
